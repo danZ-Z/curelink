dfb74fa4466b4776c07c8386d516578bc39e17fb
DanZ-Z <zuladaniel131@gmail.com>
2024-09-26 19:18:39 +0200

    Next commit

--- src/app/sync-admin-cancelled/page.tsx
+++ src/app/sync-admin-cancelled/page.tsx
@@ -1,16 +1,24 @@
-"use client"
+"use client";
 import { notFound, useSearchParams } from "next/navigation";
 import SyncPage from "./SyncPage";
+import { Suspense } from "react";
 
-const Page = () => {
+function Details() {
   const searchParams = useSearchParams();
   const appointmentId = searchParams.get("appointmentId");
-
   if (!appointmentId) {
     return notFound();
   }
 
   return <SyncPage appointmentId={appointmentId} />;
+}
+
+const Page = () => {
+  return (
+    <Suspense>
+      <Details />
+    </Suspense>
+  );
 };
 
 export default Page;
